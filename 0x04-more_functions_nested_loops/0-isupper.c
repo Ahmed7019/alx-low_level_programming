e6113eb9751e8397bf900de7362dc12462ffde3f
ahmed <ahmedabdallah7109@gmail.com>
2023-02-23 22:47:51 -0800

    Fuctions are fun

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main check the code
+ *
+ * Return 0.
+ * */
+ 
+ 
+ int _isupper(char c)
+ {
+   if (c == 'A')
+	
+	return (1);
+   else
+   { 
+   	 return(0);
+   }
+ }  
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+
+
+
+/* 
+ * _isupper : Function that checks if the character is uppercase
+ */
+
+int _isupper(char c);
+int putchar (int character);
