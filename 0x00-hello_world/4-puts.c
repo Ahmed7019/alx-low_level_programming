fb0ca29d887ac692594b68492c9ee0a4477f6c76
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-10-06 08:42:28 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int puts(const char *str);
 int main(void)
 {
 	char Mystr[] = " Programming is like building a multilingual puzzle ";
+	
 	puts(Mystr);
-	return(0);
+	return 0;
 }
