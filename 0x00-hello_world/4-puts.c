384c102d26fa102ac851e02a1c604aad32503a7d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-10-06 08:49:45 -0700

    4-puts.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-	char Mystr[] = " Programming is like building a multilingual puzzle ";
-	
+	char Mystr[50] = " Programming is like building a multilingual puzzle ";
 	puts(Mystr);
-	return 0;
+	return( 0 );
 }
