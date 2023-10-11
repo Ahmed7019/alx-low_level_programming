14971a29eac7a3c20c7fa39651be904985ed2fe3
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-10-11 11:46:16 -0700

    task 1

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * _print_alphabet - prints the alphabet in lower case
+ * @a: The alphapet to print
+ *
+ * Return: on success 0.
+ */
+
+void print_alphapet(void)
+{
+	char a;
+	putchar('%c', tolower(a));
+
+	return (0);
+}
